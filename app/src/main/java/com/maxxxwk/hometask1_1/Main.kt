1f288ac6d4969ec6d445d6616989b445c4f6116e
Maxym Vakonyuk <makswk04@gmail.com>
2020-12-01 19:30:30 +0200

    First commit

---
+++ app/src/main/java/com/maxxxwk/hometask1_1/Main.kt
@@ -0,0 +1,12 @@
+package com.maxxxwk.hometask1_1
+
+fun main(){
+    for (i in 0..100){
+        if (i % 3 == 0){
+            println("Piff")
+        }
+        if(i % 5 == 0){
+            println("Paff")
+        }
+    }
+}
